a8b621c3375ffccc29b7353acbb456d9e5231ca8
Ericmendez526 <ericmendez526@gmail.com>
2026-03-11 19:38:58 -0500

    Created a few JS validations

---
+++ HW2Folder/Homework2.js
@@ -0,0 +1,75 @@
+//First Name Validation Function
+function validateFname()
+{
+    let fname = document.getElementById("Fname").value.trim();
+    var namePattern = /^[a-zA-Z'-]+$/;
+    if (fname == "")
+        {
+            document.getElementById("fname-error").innerHTML = "First Name field cannot be empty"
+            return false;
+        }
+    else
+        if(fname != "")
+        {
+            if (!fname.match(namePattern))
+            {
+                document.getElementById("fname-error").innerHTML = "Letters, Dashes, and Apostrophes only";
+                return false;
+            }
+            else
+                if(fname.length < 2)
+                {
+                    document.getElementById("fname-error").innerHTML = "First Name cannot be less than 2 characters";
+                    return false;
+                }
+                else
+                    if(fname.length > 30)
+                    {
+                        document.getElementById("fname-error").innerHTML = "First Name connot be more than 30 Characters";
+                        return false;
+                    }
+                    else
+                    {
+                        document.getElementById("fname-error").innerHTML = ""; 
+                        return true;
+                    }
+        }
+}
+
+//Last Name Validation Function
+function validateLname()
+{
+    let lname = document.getElementById("Lname").value.trim();
+    var namePattern = /^[a-zA-Z'-]+$/;
+    if (lname == "")
+        {
+            document.getElementById("Lname-error").innerHTML = "Last Name field cannot be empty"
+            return false;
+        }
+    else
+        if(lname != "")
+        {
+            if (!lname.match(namePattern))
+            {
+                document.getElementById("Lname-error").innerHTML = "Letters, Dashes, and Apostrophes only";
+                return false;
+            }
+            else
+                if(lname.length < 2)
+                {
+                    document.getElementById("Lname-error").innerHTML = "Last Name cannot be less than 2 characters";
+                    return false;
+                }
+                else
+                    if(lname.length > 30)
+                    {
+                        document.getElementById("Lname-error").innerHTML = "Last Name cannot be more than 30 Characters";
+                        return false;
+                    }
+                    else
+                    {
+                        document.getElementById("Lname-error").innerHTML = ""; 
+                        return true;
+                    }
+        }
+}
