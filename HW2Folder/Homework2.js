84e424b218a3fcd1e9889ac9786da9ca44a3dee6
Ericmendez526 <ericmendez526@gmail.com>
2026-03-21 21:57:21 -0500

    Added more validations in JS

--- HW2Folder/Homework2.js
+++ HW2Folder/Homework2.js
@@ -39,21 +39,20 @@ function validateFname()
 //Middle Initial Validation Function
 function validateMI()
 {
-    let MI = document.getElementById("MI").value.trim();
-    var MIpattern = /^[a-zA-Z]$/;
-        if(MI != "")
-        {
-            if (!MI.match(MIpattern))
-            {
-                document.getElementById("MI-error").innerHTML = "Please enter a valid Middle Initial";
-                return false;
-            }
-            else
-            {
-                document.getElementById("MI-error").innerHTML = ""; 
-                return true;
-            }
-        }
+    let MI = document.getElementById("MI").value;
+    var namePattern = /^[A-Z]$/;
+    MI = MI.toUpperCase();
+    document.getElementById("MI").value = MI;
+    if (!MI.match(namePattern))
+    {
+        document.getElementById("MI-error").innerHTML = "Middle Initial must be a single uppercaseletter";
+        return false;
+    }
+    else
+    {
+        document.getElementById("MI-error").innerHTML = "";
+        return true;
+    }
 }
 
 //Last Name Validation Function
@@ -110,7 +109,7 @@ function validateDOB()
     else   
         if(date < new Date(maxDate))
         {
-            document.getElementById("DOB-Error").innerHTML=
+            document.getElementById("DOB-error").innerHTML=
             "Date cannot be more than 120 years ago";
             DOB.value = "";
             return false;
@@ -118,6 +117,111 @@ function validateDOB()
         else
         {
             document.getElementById("DOB-error").innerHTML= "";
+            return true;
+        }
+}
+//SSN Validation
+function validateSSN()
+{
+    const SSN = document.getElementById("SSN").value;
+    const SSNR = /^[0-9]{3}-?[0-9]{2}-?[0-9]{4}$/;
+    if(!SSNR.test(SSN))
+    {
+        document.getElementById("SSN-error").innerHTML =
+        "Please enter a valid Social Security Number";
+        return false;
+    }
+    else
+    {
+        document.getElementById("SSN-error").innerHTML = "";
+        return true;
+    }
+}
+
+//Address Validation
+function validateAL1()
+{
+    var AL1 = document.getElementById("AL1").value;
+    console.log(AL1);
+    console.log(AL1.length)
+    if(AL1.length < 2)
+        {
+            document.getElementById("AL1-error").innerHTML=
+            "Please enter a valid address.";
+            return false;
+        }
+    else 
+        {
+            document.getElementById("AL1-error").innerHTML = "";
+            return true;
+        }   
+}
+
+//City Validation
+function validateCity()
+{
+    let City = document.getElementById("City").value.trim();
+    if(!City)
+    {
+        document.getElementById("City-error").innerHTML = 
+        "City cannot be blank";
+        return false;
+    }
+    else
+    {
+        document.getElementById("City-error").innerHTML = "";
+        return true; 
+    }
+}
+
+// ZipCode Validation
+function validateZipCode()
+{
+    const zipInput = document.getElementById("ZipCode");
+    let ZipCode = zipInput.value.replace(/[^\d-]/g, "");
+
+    if(!ZipCode)
+    {
+        document.getElementById("ZipCode-error").innerHTML = 
+        "Zip code cannot be blank";
+        return false;
+    }
+        if (ZipCode.length > 5)
+        {
+            ZipCode = ZipCode.slice(0,5) + "-" + ZipCode.slice(5,9);
+        }
+        else
+        {
+            ZipCode = ZipCode.slice(0,5);
+        }
+
+        zipInput.value = ZipCode;
+        document.getElementById("ZipCode-error")/innerHTML = "";
+        return true;
+}
+
+//Email Validation
+function validateEmail
+{
+    let email = document.getElementById("Email");
+    var emailR = /^\w+([.-]?\w+)@\w+([.-]?\w+)(.\w{2,3})+$/;
+    
+    if(email == "")
+    {
+        document.getElementById("Email-error").innerHTML = 
+        "Email Address cannot be empty";
+        return false;
+    }
+    else
+        if(!email.match(emailR))
+        {
+            document.getElementById("Email-error")=
+            "Please enter a valid email address.";
+            return false;
+        }   
+        else
+        {
+            docuemtn.getElementById("Email-error") = "";
             return true;
         }
 }
